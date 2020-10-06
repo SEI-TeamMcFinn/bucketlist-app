242e665ccff7cf3c2f6f2c3fda91ae657e66a538
mchandonnet <mike@chandon.net>
2020-10-06 16:48:35 -0400

    Added Bucket List Functionality
    Group Code:  @brhukee, @Samuel-Hawkins
    Created Paths, forms and Actions

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import { Nav, Navbar } from 'react-bootstrap'
+import bucket from './../../public/images/bucket.png'
 
 const authenticatedOptions = (
   <Fragment>
@@ -18,19 +18,19 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link to="/">Home</Nav.Link>
+    <Nav.Link href="#buckets">Home</Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
     <Navbar.Brand href="#">
-      bucketlist-app
+      <img src={bucket} width="10%" height="10%" />
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { user && <span className="navbar-text mr-2">Welcome, {user.firstName}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -33,7 +33,7 @@ class SignIn extends Component {
         message: messages.signInSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/buckets'))
       .catch(error => {
         this.setState({ email: '', password: '' })
         msgAlert({
