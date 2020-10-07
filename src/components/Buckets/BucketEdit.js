af1521bb9f63710401bf3763c3e047e4c799b762
mchandonnet <mike@chandon.net>
2020-10-07 09:08:32 -0400

    Edit Button
    Add Edit button, and build initial form fields

--- src/components/Buckets/BucketEdit.js
+++ src/components/Buckets/BucketEdit.js
@@ -55,7 +55,7 @@ class EditBucketItem extends Component {
   }
 
   render () {
-    console.log(this.props)
+    console.log('Props: ', this.props)
     if (this.state.createdBucketItemId) {
       return <Redirect to={'/buckets/'} />
     }
